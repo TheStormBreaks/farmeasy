0c65790de16e81470bbf2931a1bea0e723a3ac3d
Somya Jha <sumicode.exe@gmail.com>
2025-05-23 16:50:10 +0000

    can you add the gradient you have in home page, in all the pages as well ?

--- src/app/supply/layout.tsx
+++ src/app/supply/layout.tsx
@@ -7,7 +7,7 @@ export default function SupplyLayout({ children }: { children: ReactNode }) {
   return (
     <AuthGuard requiredUserType="SUPPLY">
         <AppHeader userRole="SUPPLY" />
-        <main className="p-4 md:p-8">
+        <main className="p-4 md:p-8 min-h-[calc(100vh-3.5rem)] bg-gradient-to-br from-primary/10 via-background to-secondary/10">
           {children}
         </main>
     </AuthGuard>
