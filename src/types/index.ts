608c779200a6eb4736e442c3a8720c57846dc6fe
Somya Jha <sumicode.exe@gmail.com>
2025-05-23 17:04:33 +0000

    now implement "proceed to checkout" option. when adding products, take the "UPI id" of the supplier. then when farmer does proceed to checkout, take it to the razorpay page where the payment will be done to the upi entered by the supplier

--- src/types/index.ts
+++ src/types/index.ts
@@ -16,6 +16,7 @@ export interface Product {
     price: number; // Store price as a number
     availableQuantity: number;
     supplierId: string; // Could be 'SUPPLY' for now, or a specific user ID if multiple suppliers
+    supplierUpiId: string; // UPI ID of the supplier for this product
     createdAt: number; // Timestamp
     updatedAt: number; // Timestamp
 }
